3c524615734b7185715dc98dcdbc043b85735d41
bamba100000 <nafissatou3582@gmail.com>
2025-07-28 19:35:23 +0000

    Mise à jour render.yaml : configuration Railway pour Render

---
+++ public/test.php
@@ -0,0 +1,8 @@
+<?php
+echo '<pre>';
+echo "DB_HOST = " . getenv('DB_HOST') . "\n";
+echo "DB_NAME = " . getenv('DB_NAME') . "\n";
+echo "DB_USER = " . getenv('DB_USER') . "\n";
+echo "DB_PASS = " . getenv('DB_PASS') . "\n";
+echo "MYSQL_PORT = " . getenv('MYSQL_PORT') . "\n";
+echo '</pre>';
